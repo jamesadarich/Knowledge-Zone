c4dc0cfece402ee20ad99ac8918c913544a368e8
jamesrichford <jamesrichford@googlemail.com>
2015-07-09 22:55:58 +0100

    Auto tinker

--- backbone/js/modules/models/base.js
+++ backbone/js/modules/models/base.js
@@ -2,10 +2,40 @@ define(['backbone'],
   function(Backbone){
 
     var ModelsView = Backbone.View.extend({
+      events: {
+        'keydown .function-view': 'execute',
+        'keydown .input-view': 'execute'
+      },
+
+      initialize: function() {
+        var Model = Backbone.Model.extend({});
+        this.model = new Model();
+        this.model.set('name', 'test');
+        this.model.set('other', 'thing');
+      },
     	render: function() {
-        this.el.innerText = 'models';
-    		//this.$el.html("<h1>This is the home page</hove1><a href='#users'>Go to users</a>");
-    	}
+    		this.$el.html('<textarea class="input-view"></textarea><textarea class="function-view"></textarea><div class="output-view"></div>');
+
+        this.$el.find('.input-view').text(JSON.stringify(this.model.toJSON(), null, 2));
+        this.$el.find('.function-view').text('return model.get(\'name\')');
+    	},
+
+      execute: function() {
+              try {
+        var f = new Function('model', this.$el.find('.function-view').val());
+
+        var m = Backbone.Model.extend({
+          defaults: {
+            'name': 'test'
+          }
+        });
+
+          this.$el.find('.output-view').text(f(this.model));
+        }
+        catch (e) {
+          this.$el.find('.output-view').text(e);
+        }
+      }
     });
 
     return ModelsView;
