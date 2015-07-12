3b75310f4d921aedf99d22361fefd6840749e0dd
jamesrichford <jamesrichford@googlemail.com>
2015-07-12 12:22:17 +0100

    install rimraf globally

--- deploy.cmd
+++ deploy.cmd
@@ -98,7 +98,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 call :SelectNodeVersion
 
 :: 3. Clean bower_components and node_modules
-$NPM_CMD install rimraf
+$NPM_CMD install rimraf -g
 rimraf ./node_modules
 rimraf ./bower_components
 
